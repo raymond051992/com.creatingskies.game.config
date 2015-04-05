310088ee6640be58c0ccdf842cdb707f0f96a7e4
jahnavarro <ganjaboi.navarro@gmail.com>
2015-04-05 09:02:39 +0800

    added obstacle dialog fxml and controller. modification on obstacles view.

---
+++ src/main/java/com/creatingskies/game/config/obstacle/ObstacleDialogController.java
@@ -0,0 +1,167 @@
+package com.creatingskies.game.config.obstacle;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+import com.creatingskies.game.classes.ViewController;
+import com.creatingskies.game.common.AlertDialog;
+import com.creatingskies.game.common.MainLayout;
+import com.creatingskies.game.model.obstacle.Obstacle;
+import com.creatingskies.game.model.type.GameType;
+import com.creatingskies.game.model.type.GameTypeDAO;
+
+public class ObstacleDialogController extends ViewController {
+
+	@FXML private TextField nameField;
+	@FXML private ChoiceBox<GameType> gameTypeChoices;
+	@FXML private Slider slider;
+	
+	private GameTypeDAO gameTypeDAO;
+	
+	private Stage dialogStage;
+	private Obstacle obstacle;
+	private boolean saveClicked = false;
+	
+	public boolean show(Obstacle obstacle) {
+	    try {
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(getClass().getResource("ObstacleDialog.fxml"));
+	        AnchorPane page = (AnchorPane) loader.load();
+
+	        Stage dialogStage = new Stage();
+	        dialogStage.setTitle("Obstacle");
+	        dialogStage.initModality(Modality.WINDOW_MODAL);
+	        dialogStage.initOwner(MainLayout.getPrimaryStage());
+	        Scene scene = new Scene(page);
+	        dialogStage.setScene(scene);
+
+	        ObstacleDialogController controller = loader.getController();
+	        controller.setDialogStage(dialogStage);
+	        controller.setObstacle(obstacle);
+	        
+	        dialogStage.showAndWait();
+	        return controller.isSaveClicked();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
+	
+	@Override
+	public void initialize() {
+		super.initialize();
+		gameTypeDAO = new GameTypeDAO();
+		
+		gameTypeChoices.setConverter(new StringConverter<GameType>() {
+			@Override
+			public String toString(GameType object) {
+				return object.getName();
+			}
+			
+			@Override
+			public GameType fromString(String name) {
+				return gameTypeDAO.findGameTypeByName(name);
+			}
+		});
+		
+		gameTypeChoices.setItems(FXCollections
+				.observableArrayList(gameTypeDAO.findAll()));
+		
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setMin(0);
+		slider.setMajorTickUnit(1);
+		slider.setMax(7);
+	}
+
+	public void setObstacle(Obstacle obstacle) {
+        this.obstacle = obstacle;
+        nameField.setText(obstacle.getName());
+        
+        slider.setValue(obstacle.getDifficulty() != null ?
+        		obstacle.getDifficulty() : 0.0);
+        
+        if(obstacle.getGameType() != null){
+        	gameTypeChoices.setValue(obstacle.getGameType());
+        }
+    }
+	
+	public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+	
+	public boolean isSaveClicked() {
+        return saveClicked;
+    }
+	
+	@Override
+	protected String getViewTitle() {
+		return "Obstacles";
+	}
+	
+	@FXML
+	private void handleOpenBrowseDialog(){
+		FileChooser fileChooser = new FileChooser();
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "Image files", "*.jpeg", "*.jpg", "*.png", "*.bmp", "*.gif");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File file = fileChooser.showOpenDialog(MainLayout.getPrimaryStage());
+
+        if (file != null) {
+            new AlertDialog(AlertType.INFORMATION, "Todo!", "Todo: Under construction.",
+            		file.getAbsolutePath()).show();;
+        }
+	}
+	
+	@FXML
+    private void handleSave() {
+        if (isInputValid()) {
+            obstacle.setName(nameField.getText());
+            obstacle.setDifficulty((int) slider.getValue());
+            obstacle.setGameType(gameTypeChoices.getValue());
+            saveClicked = true;
+            dialogStage.close();
+        }
+    }
+	
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+    
+    private boolean isInputValid() {
+    	//TODO Add validation
+    	
+        String errorMessage = "";
+
+        if (nameField.getText() == null || nameField.getText().length() == 0) {
+            errorMessage += "No obstacle name!\n"; 
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+			new AlertDialog(AlertType.ERROR, "Invalid fields",
+					"Please correct invalid fields.", errorMessage)
+					.showAndWait();
+            return false;
+        }
+    }
+
+}
--- src/main/java/com/creatingskies/game/config/obstacle/ObstaclesController.java
+++ src/main/java/com/creatingskies/game/config/obstacle/ObstaclesController.java
@@ -1,16 +1,20 @@
 package com.creatingskies.game.config.obstacle;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 import com.creatingskies.game.classes.TableViewController;
+import com.creatingskies.game.common.AlertDialog;
 import com.creatingskies.game.common.MainLayout;
 import com.creatingskies.game.model.IRecord;
 import com.creatingskies.game.model.obstacle.Obstacle;
@@ -20,7 +24,11 @@ public class ObstaclesController extends TableViewController{
 
 	@FXML private TableView<Obstacle> obstaclesTable;
 	@FXML private TableColumn<Obstacle, String> nameColumn;
+	@FXML private TableColumn<Obstacle, String> difficultyColumn;
+	@FXML private TableColumn<Obstacle, String> gameTypeColumn;
 	@FXML private TableColumn<Obstacle, Object> actionColumn;
+	
+	private ObstacleDAO obstacleDAO;
 	
 	public void show(){
 		try {
@@ -37,14 +45,24 @@ public class ObstaclesController extends TableViewController{
 	@SuppressWarnings("unchecked")
 	public void initialize(){
 		super.initialize();
+		obstacleDAO = new ObstacleDAO();
+		
 		nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(
 				cellData.getValue().getName()));
 		
+		difficultyColumn.setCellValueFactory(cellData -> new SimpleStringProperty(
+				cellData.getValue().getDifficulty() != null ? cellData
+						.getValue().getDifficulty().toString() : null));
+		
+		gameTypeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(
+				cellData.getValue().getGameType() != null ? cellData
+						.getValue().getGameType().getName() : null));
+		
 		actionColumn.setCellFactory(generateCellFactory(Action.EDIT, Action.DELETE));
-		resetTableView(new ObstacleDAO());
+		resetTableView();
 	}
 	
-	private void resetTableView(ObstacleDAO obstacleDAO){
+	private void resetTableView(){
 		obstaclesTable.setItems(FXCollections
 				.observableArrayList(obstacleDAO.findAll()));
 	}
@@ -57,6 +75,44 @@ public class ObstaclesController extends TableViewController{
 	@Override
 	public TableView<? extends IRecord> getTableView() {
 		return obstaclesTable;
+	}
+	
+	@FXML
+	private void handleAdd() {
+		showDetailDialog(new Obstacle());
+	}
+
+	@Override
+	protected void editRecord(IRecord record) {
+		super.editRecord(record);
+		
+		if(record instanceof Obstacle){
+			showDetailDialog((Obstacle) record);
+		}
+	}
+	
+	private void showDetailDialog(Obstacle obstacle){
+		boolean saveClicked = new ObstacleDialogController().show(obstacle);
+	    if (saveClicked) {
+	        obstacleDAO.saveOrUpdate(obstacle);
+	        resetTableView();
+	    }
+	}
+
+	@Override
+	protected void deleteRecord(IRecord record) {
+		super.deleteRecord(record);
+		Optional<ButtonType> result = new AlertDialog(AlertType.CONFIRMATION, "Confirmation Dialog",
+				"Are you sure you want to delete this obstacle?", null).showAndWait();
+		
+		if(result.get() == ButtonType.OK){
+			try {
+				obstacleDAO.delete(record);
+				resetTableView();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
